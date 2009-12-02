f1a64b7c3ac6106130b13f121f87ea6e866a8318
thomas.diesler@jboss.com <thomas.diesler@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2009-12-02 14:52:15 +0000

    Use deployment-1.0.0-SNAPSHOT
    git-svn-id: https://anonsvn.jboss.org/repos/jbossas/projects/jboss-osgi/projects/runtime/equinox/trunk@97294 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/java/org/jboss/osgi/equinox/EquinoxIntegration.java
+++ src/main/java/org/jboss/osgi/equinox/EquinoxIntegration.java
@@ -25,7 +25,7 @@ package org.jboss.osgi.equinox;
 
 import java.util.Map;
 
-import org.jboss.osgi.deployment.DeploymentActivator;
+import org.jboss.osgi.deployment.DeploymentServicesActivator;
 import org.jboss.osgi.spi.framework.FrameworkIntegrationBean;
 import org.jboss.osgi.spi.util.ServiceLoader;
 import org.osgi.framework.BundleContext;
@@ -45,7 +45,7 @@ public class EquinoxIntegration extends FrameworkIntegrationBean
    // Provide logging
    final Logger log = LoggerFactory.getLogger(EquinoxIntegration.class);
    
-   private DeploymentActivator deploymentActivator;
+   private DeploymentServicesActivator deploymentActivator;
 
    @Override
    protected Framework createFramework(Map<String, Object> properties)
@@ -63,7 +63,7 @@ public class EquinoxIntegration extends FrameworkIntegrationBean
    @Override
    protected void registerSystemServices(BundleContext context)
    {
-      deploymentActivator = new DeploymentActivator();
+      deploymentActivator = new DeploymentServicesActivator();
       deploymentActivator.start(context);
    }
 
